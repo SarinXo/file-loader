bf73c62dadbd2ca28dc3463e9cb705dafeae0139
EgorovIA <rollovitch.roll@yandex.ru>
2024-05-12 20:39:25 +0300

    v_1_0_0

--- src/main/java/ilya/coursework/fileloader/controller/rest/FileController.java
+++ src/main/java/ilya/coursework/fileloader/controller/rest/FileController.java
@@ -5,9 +5,11 @@ import ilya.coursework.fileloader.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,33 +19,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
 @Service
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
 public class FileController {
     private final FileService fileServiceImpl;
 
     @PostMapping
-    @ResponseBody
     public UUID upload(@RequestBody MultipartFile file) {
         return fileServiceImpl.save(file);
     }
 
     @GetMapping("/{id}")
-    public HttpEntity<ByteArrayResource> download(@PathVariable UUID id) {
+    public ResponseEntity<ByteArrayResource> download(@PathVariable UUID id) {
         val fileEntity = fileServiceImpl.getById(id);
         val resource = new ByteArrayResource(fileEntity.getFile());
         val headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        headers.setContentDispositionFormData("file", fileEntity.getFileName());
+
+        val contentDisposition = ContentDisposition.builder("attachment")
+                .filename(fileEntity.getFileName(), StandardCharsets.UTF_8)
+                .build();
+
+        headers.setContentDisposition(contentDisposition);
         headers.setContentLength(fileEntity.getFile().length);
-        return new HttpEntity<>(resource, headers);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
     }
 
     @GetMapping
